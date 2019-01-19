46773011a772f89006ce4c8f4519d4c6683435b6
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-19 05:50:48 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,10 @@
 def hello_t(names)
-  names.each { |name| puts "#{name}"}
+# names.each { |name| puts "#{name}"}
+i = 0
+    while i < names.length
+      i = i + 1
+    end
+    puts "#{name}"
 end
 
 # call your method here!
