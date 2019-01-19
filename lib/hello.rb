3d25138bca68f67c598ce60befa4c095170fbbda
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-19 05:59:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,15 @@
 def hello_t(names)
 # names.each { |name| puts "#{name}"}
+if block_given?
 i = 0
+
     while i < names.length
+      yield(names[i])
       i = i + 1
     end
     names
+  else 
+    puts "Hey! No block was given!"
 end
 
 # call your method here!
